f4b1d1d95c8968aed36aa89963bb85950072a15a
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-04 04:24:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,15 +5,11 @@ def countdown(num)
 
   while num > 0
 
-    #if num == 2
-    #  greet=   "HAPPY NEW YEAR!"
-    #end
-   puts  " #{num} SECOND(S) !"
+   puts  " #{num} SECOND(S)!"
    num -= 1
 
   end
-  return "HAPPY NEW YEAR!"
-
+return "HAPPY NEW YEAR!"
 
 end
 
